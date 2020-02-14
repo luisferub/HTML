7fa678159fdb99fbaa4aee825851231a84fa1a5e
Luis Fernando Uribe <luisferub@gmail.com>
2020-02-13 20:52:08 -0500

    version 1.0

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+
+// Project configuration.
+grunt.initConfig({
+	pkg: grunt.file.readJSON('package.json'),
+	
+	cssmin: {
+		my_target: {
+			files: [{ 
+				expand: true,
+				cwd: 'css/',
+				src: ['*.css','!*.min.css'],
+				dest: 'css/',
+				ext: '.min.css'
+			}]
+		}
+	}
+
+	});
+	
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	
+};
